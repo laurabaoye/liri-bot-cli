86eaadebf51fac2ebc795ce6f7578e92439ee133
laurabaoye <laurabaoye@gmail.com>
2020-03-27 20:00:32 -0700

    uploading to git hub

---
+++ app.js
@@ -0,0 +1,54 @@
+const Spotify = require("node-spotify-api");
+const omdb = require("omdb-client");
+const axios = require("axios");
+
+require("dotenv").config();
+// console.log(process.env.SPOTIFY_ID);
+
+let spotify = new Spotify({
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET,
+})
+
+let action = process.argv[2];
+let command = process.argv.splice(3).join(" ");
+// console.log(command, action);
+
+if (action === "spotify") {
+    console.log("spotify search: ", command);
+    spotify.search({
+        query: command,
+        type: "track",
+        limit: 1
+    }, (error, data) => {
+        // console.log(data.tracks.items[0]);
+        console.log({
+            artist: data.artists[0].name,
+            // song: data.artists[0].name,
+            // album: data.,
+            // url: data.artist[0].url
+        });
+
+    })
+} else if (action === "omdb") {
+    console.log("omdb search: ", command);
+    omdb.get({
+        apiKey: process.env.OMDB_KEY,
+        title: command
+    }, (error, data) => {
+        // console.log(data);
+        console.log({
+            title: data.Title,
+            year: data.Year,
+            rating: data.imdbRating,
+            // location:,
+            // langusage:,
+            // plots:,
+            // actors:
+        });
+
+    })
+} else {
+    console.log("no results");
+}
+
